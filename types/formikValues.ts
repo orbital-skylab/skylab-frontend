f176bf90866a96adcf58e93f8e5aa5a393aa7022
raynerljm <raynerljm@gmail.com>
2022-05-19 23:01:34 +0800

    fix: remove old formik type folder pre-generics

--- types/formikValues.ts
+++
@@ -1,9 +0,0 @@
-export type AllFormikValues = FormikSignInValues | TestValues;
-export interface FormikSignInValues {
-  email: string;
-  password: string;
-}
-
-export interface TestValues {
-  test: string;
-}
