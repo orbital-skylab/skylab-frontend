1d1f5e1144bc8e93048955d7c9ab7816376cde6f
raynerljm <raynerljm@gmail.com>
2022-05-22 00:08:20 +0800

    refactor: rename helper function file

---
+++ helpers/api.ts
@@ -0,0 +1,44 @@
+import { string } from "yup";
+
+/**
+ * Constants
+ */
+export const API_URL = `${process.env.NEXT_PUBLIC_BASE_API_URL}`;
+
+/**
+ * Helper functions
+ */
+export async function PostApiService(
+  url: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  body: { [key: string]: any }
+) {
+  const res = await fetch(`${API_URL}${url}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${}`
+    },
+    body: JSON.stringify(body),
+  });
+  return res;
+}
+
+
+export function curryPostApiService(url: string, body: {[key: string]: any}) {
+    function createServiceWithToken(token: string) {
+        async function postApiService() {
+            const res = await fetch(`${API_URL}${url}`, {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                  authorization: `Bearer ${token}`
+                },
+                body: JSON.stringify(body),
+              });
+              return res;
+        }
+        return postApiService;
+    }
+    return createServiceWithToken;
+}
