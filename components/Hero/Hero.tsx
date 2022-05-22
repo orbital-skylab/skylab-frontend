c3cb1e0a3f1edda47f135804a0482d4e9f40a6ad
raynerljm <raynerljm@gmail.com>
2022-05-22 21:07:05 +0800

    feat: create starfield landing component

---
+++ components/Hero/Hero.tsx
@@ -0,0 +1,26 @@
+import { Box } from "@mui/material";
+import { FC } from "react";
+import styles from "./Hero.module.css";
+
+const Hero: FC = () => {
+  return (
+    <Box
+      sx={{
+        backgroundColor: "primary.main",
+        position: "relative",
+        top: { xs: "-56px", sm: "-64px", md: "-68.5px" },
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <div className={styles.space}>
+        <div className={styles.stars}></div>
+        <div className={styles.stars}></div>
+        <div className={styles.stars}></div>
+        <div className={styles.stars}></div>
+        <div className={styles.stars}></div>
+      </div>
+    </Box>
+  );
+};
+export default Hero;
