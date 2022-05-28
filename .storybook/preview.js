5d05117adb17b4efa78d077321cf29e799e90931
raynerljm <raynerljm@gmail.com>
2022-05-28 17:22:28 +0800

    docs: initialize more documentation pages

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -8,6 +8,27 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  options: {
+    storySort: {
+      order: [
+        "Introduction",
+        "Documentation",
+        [
+          "Folder Structure",
+          "Tech Stack",
+          "Abstractions",
+          "ESLint and Prettier",
+          "Depedendencies",
+          "Features",
+          "Non Functional Requirements",
+        ],
+        "Components",
+        ["Storybook"],
+        "Timeline and Workflow",
+        ["Team Structure", "Issue Tracking", "Git Workflow", "Conventions"],
+      ],
+    },
+  },
 };
 
 export const decorators = [muiTheme()];
