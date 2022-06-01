3f2f0250cdd1254d980cc9dd1f08eb3d41ae73c5
myc37 <nvjn37@gmail.com>
2022-06-01 23:43:52 +0800

    chore: add constants for projects

---
+++ constants/Projects.constants.ts
@@ -0,0 +1,13 @@
+export const placeholderProjects = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+export const levelsOfAchievement = ["Artemis", "Apollo", "Gemini", "Vostok"];
+
+export const cohorts = [2022, 2021, 2020];
+
+export const getCurrentCohort = () => {
+  return cohorts[0];
+};
+
+export const getHighestLevelOfAchievement = () => {
+  return levelsOfAchievement[0];
+};
