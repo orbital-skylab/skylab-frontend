7cae9d343ac2298980aea61d4fa8baf2fe576148
myc37 <nvjn37@gmail.com>
2022-06-01 23:48:42 +0800

    chore: add constants for staff

---
+++ constants/Staff.constants.ts
@@ -0,0 +1,14 @@
+export const placeholderStaff = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+];
+
+export const staffTypes = ["Facilitators", "Advisors", "Mentors", "Tutors"];
