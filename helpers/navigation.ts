c332ea90104ecdb7602dce5e1027ad9bffc2891d
myc37 <nvjn37@gmail.com>
2022-06-02 00:51:30 +0800

    feat: add navigation option for staff page

--- helpers/navigation.ts
+++ helpers/navigation.ts
@@ -1,3 +1,7 @@
 export const LANDING_PAGE = "/";
 export const PROJECTS_PAGE = "/projects";
-export const NAVBAR_OPTIONS = [{ label: "Projects", route: PROJECTS_PAGE }];
+export const STAFF_PAGE = "/staff";
+export const NAVBAR_OPTIONS = [
+  { label: "Projects", route: PROJECTS_PAGE },
+  { label: "Staff", route: STAFF_PAGE },
+];
