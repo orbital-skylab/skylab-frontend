11f2975fca54c15b21e7582bc24cf312432e65ea
myc37 <nvjn37@gmail.com>
2022-06-02 22:03:33 +0800

    chore: remove constants add add enums for projects and staff

---
+++ types/projects.ts
@@ -0,0 +1,11 @@
+export enum LEVELS_OF_ACHIEVEMENT {
+  ARTEMIS = "Artemis",
+  APOLLO = "Apollo",
+  GEMINI = "Gemini",
+  VOLSTOK = "Volstok",
+}
+
+export enum COHORTS {
+  CURRENT = 2022,
+  PREVIOUS = 2021,
+}
