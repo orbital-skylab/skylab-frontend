a2fd0d13726db54a07fc33d1e427008e8cce6a25
myc37 <nvjn37@gmail.com>
2022-06-04 03:49:13 +0800

    fix: remove tutors from staff types

--- types/staff.ts
+++ types/staff.ts
@@ -2,7 +2,6 @@ export enum STAFF_TYPES {
   FACILITATORS = "Facilitators",
   MENTORS = "Mentors",
   ADVISERS = "Advisers",
-  TUTORS = "Tutors",
 }
 
 export const STAFF_VALUES = Object.values(STAFF_TYPES);
