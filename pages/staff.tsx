dc520a375561f56d6248d6abe2e640d688fad78f
myc37 <nvjn37@gmail.com>
2022-06-04 14:41:14 +0800

    feat: add social buttons to staff card and remove CTA to view profile

--- pages/staff.tsx
+++ pages/staff.tsx
@@ -1,23 +1,34 @@
 import { SyntheticEvent, useMemo, useState } from "react";
 import type { NextPage } from "next";
 // Libraries
-import { Grid, MenuItem, Select, Stack, Tab, Tabs } from "@mui/material";
+import {
+  Grid,
+  MenuItem,
+  Select,
+  Stack,
+  Tab,
+  Tabs,
+  tabsClasses,
+  TextField,
+} from "@mui/material";
 // Components
 import Body from "@/components/Body";
 import StaffCard from "@/components/cards/StaffCard";
 // Constants
-import { STAFF_TYPES, STAFF_VALUES } from "@/types/staff";
+import { STAFF_VALUES } from "@/types/staff";
 import useFetch, { FETCH_STATUS } from "@/hooks/useFetch";
 import NoDataWrapper from "@/components/wrappers/NoDataWrapper";
 import NoStaffFound from "@/components/emptyStates/NoStaffFound";
 import { User } from "@/types/users";
 import { COHORTS, COHORTS_VALUES } from "@/types/cohorts";
+import LoadingWrapper from "@/components/wrappers/LoadingWrapper";
 
 const Staff: NextPage = () => {
+  const [searchTextInput, setSearchTextInput] = useState<string>("");
   const [selectedCohort, setSelectedCohort] = useState<COHORTS>(
     COHORTS_VALUES[0]
   );
-  const [selectedType, setSelectedType] = useState<string>(STAFF_VALUES[1]);
+  const [selectedType, setSelectedType] = useState<string>(STAFF_VALUES[0]);
   const memoQueryParams = useMemo(() => {
     return {
       cohortYear: selectedCohort,
@@ -33,29 +44,31 @@ const Staff: NextPage = () => {
     setSelectedType(newType);
   };
 
-  console.log(staff);
-
   return (
     <>
-      <Body
-        isError={status === FETCH_STATUS.ERROR}
-        isLoading={status === FETCH_STATUS.FETCHING}
-      >
-        <NoDataWrapper
-          noDataCondition={staff === undefined}
-          fallback={<NoStaffFound />}
-        >
+      <Body isError={status === FETCH_STATUS.ERROR}>
+        <Stack direction="column" mt={{ md: "0.5rem" }} mb="1rem">
           <Stack
-            direction={{ xs: "column", md: "row" }}
-            alignItems="center"
+            direction="row"
             justifyContent="space-between"
-            my={4}
+            alignItems="center"
+            width="100%"
+            mb={{ md: "0.5rem" }}
           >
+            <TextField
+              label="Search"
+              value={searchTextInput}
+              onChange={(e) => {
+                setSearchTextInput(e.target.value);
+              }}
+              size="small"
+            />
             <Select
               name="cohort"
               label="Cohort"
               value={selectedCohort}
               onChange={(e) => setSelectedCohort(e.target.value as COHORTS)}
+              size="small"
             >
               {COHORTS_VALUES.map((value) => (
                 <MenuItem key={value} value={value}>
@@ -63,40 +76,51 @@ const Staff: NextPage = () => {
                 </MenuItem>
               ))}
             </Select>
-            <Tabs
-              value={selectedType}
-              onChange={handleTabChange}
-              textColor="secondary"
-              indicatorColor="secondary"
-              aria-label="project-level-tabs"
-              variant="scrollable"
-              scrollButtons="auto"
-              allowScrollButtonsMobile
-            >
-              {Object.values(STAFF_TYPES).map((type) => {
-                return <Tab key={type} value={type} label={type} />;
-              })}
-            </Tabs>
           </Stack>
-          <Grid
-            container
-            sx={{ margin: "auto" }}
-            spacing={{ xs: 2, md: 4, xl: 8 }}
-            pb="1rem"
+          <Tabs
+            value={selectedType}
+            onChange={handleTabChange}
+            textColor="secondary"
+            indicatorColor="secondary"
+            aria-label="project-level-tabs"
+            variant="scrollable"
+            scrollButtons="auto"
+            allowScrollButtonsMobile
+            sx={{
+              maxWidth: { xs: "100%", md: "75%" },
+              [`& .${tabsClasses.scrollButtons}`]: { color: "primary" },
+              marginY: { xs: 2, md: 0 },
+            }}
           >
-            {staff
-              ? staff.map((person) => {
-                  return (
-                    <Grid item key={person.id} xs={12} md={4} xl={3}>
-                      <StaffCard staff={person} />
-                    </Grid>
-                  );
-                })
-              : null}
-          </Grid>
-        </NoDataWrapper>
+            {STAFF_VALUES.map((level) => {
+              return <Tab key={level} value={level} label={level} />;
+            })}
+          </Tabs>
+        </Stack>
+        <LoadingWrapper
+          isLoading={status === FETCH_STATUS.FETCHING}
+          loadingText="Loading projects..."
+        >
+          <NoDataWrapper
+            noDataCondition={staff === undefined || staff?.length === 0}
+            fallback={<NoStaffFound />}
+          >
+            <Grid container spacing={{ xs: 2, md: 4, xl: 8 }} pb={"2rem"}>
+              {staff
+                ? staff.map((person) => {
+                    return (
+                      <Grid item key={person.id} xs={6} md={3} xl={12 / 5}>
+                        <StaffCard staff={person} />
+                      </Grid>
+                    );
+                  })
+                : null}
+            </Grid>
+          </NoDataWrapper>
+        </LoadingWrapper>
       </Body>
     </>
   );
 };
+
 export default Staff;
