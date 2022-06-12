c29abf8b4a92aeb3091a8a2419e6d1696c17da39
raynerljm <raynerljm@gmail.com>
2022-06-12 17:23:58 +0800

    test: add unit tests for new helper functions

--- hooks/useFetch/useFetch.test.js
+++ hooks/useFetch/useFetch.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
-import { parseQueryParams } from "./useFetch.helpers";
+import { parseQueryParams, isFetching, isError } from "./useFetch.helpers";
 import "jest";
+import { FETCH_STATUS } from "./useFetch.types";
 
 describe("#parseQueryParams", () => {
   it("can transform zero params", () => {
@@ -91,5 +92,57 @@ describe("#parseQueryParams", () => {
     });
     const expected = "?shdExist1=0&shdExist2=0&shdExist3=1&shdExist4=1";
     expect(parsed).toBe(expected);
+  });
+});
+
+describe("#isFetching", () => {
+  it("returns true with one status", () => {
+    expect(isFetching(FETCH_STATUS.FETCHING)).toBeTruthy();
+  });
+
+  it("returns true with multiple statuses where one is fetching", () => {
+    expect(
+      isFetching(FETCH_STATUS.FETCHING, FETCH_STATUS.FETCHED)
+    ).toBeTruthy();
+  });
+
+  it("returns true with multiple statuses where all is fetching", () => {
+    expect(
+      isFetching(FETCH_STATUS.FETCHING, FETCH_STATUS.FETCHING)
+    ).toBeTruthy();
+  });
+
+  it("returns false with one status", () => {
+    expect(isFetching(FETCH_STATUS.FETCHED)).toBeFalsy();
+  });
+
+  it("returns false with multiple statuses where none is fetching", () => {
+    expect(
+      isFetching(FETCH_STATUS.FETCHED, FETCH_STATUS.ERROR, FETCH_STATUS.IDLE)
+    ).toBeFalsy();
+  });
+});
+
+describe("#isError", () => {
+  it("returns true with one status", () => {
+    expect(isError(FETCH_STATUS.ERROR)).toBeTruthy();
+  });
+
+  it("returns true with multiple statuses where one is error", () => {
+    expect(isError(FETCH_STATUS.ERROR, FETCH_STATUS.FETCHED)).toBeTruthy();
+  });
+
+  it("returns true with multiple statuses where all is error", () => {
+    expect(isError(FETCH_STATUS.ERROR, FETCH_STATUS.ERROR)).toBeTruthy();
+  });
+
+  it("returns false with one status", () => {
+    expect(isError(FETCH_STATUS.FETCHED)).toBeFalsy();
+  });
+
+  it("returns false with multiple statuses where none is error", () => {
+    expect(
+      isError(FETCH_STATUS.FETCHED, FETCH_STATUS.FETCHING, FETCH_STATUS.IDLE)
+    ).toBeFalsy();
   });
 });
