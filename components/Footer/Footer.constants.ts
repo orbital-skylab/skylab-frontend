5fad1b81df8eb32097d8f730c33e4877a73b7171
raynerljm <raynerljm@gmail.com>
2022-06-12 16:59:06 +0800

    feat: add links to footer

---
+++ components/Footer/Footer.constants.ts
@@ -0,0 +1,4 @@
+export const NUS_SOC = "https://www.comp.nus.edu.sg/";
+export const ORBITAL_WORDPRESS = "https://orbital.comp.nus.edu.sg/";
+export const SKYLAB_FRONTEND_ISSUES =
+  "https://github.com/orbital-skylab/skylab-frontend/issues";
--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,14 +1,27 @@
-import { Box, Container, Stack, Typography } from "@mui/material";
+import { Box, Container, Link, Stack, Typography } from "@mui/material";
 import { FC } from "react";
+import {
+  NUS_SOC,
+  ORBITAL_WORDPRESS,
+  SKYLAB_FRONTEND_ISSUES,
+} from "./Footer.constants";
 
 const Footer: FC = () => {
   return (
     <Box sx={{ backgroundColor: "primary.main" }}>
       <Container maxWidth="xl" sx={{ paddingX: "1rem" }}>
         <Stack direction="row" justifyContent="space-between" paddingY="1rem">
-          <Typography color="primary.contrastText">©NUS SoC</Typography>
-          <Typography color="primary.contrastText">Orbital Program</Typography>
-          <Typography color="primary.contrastText">Report Issues</Typography>
+          <Link href={NUS_SOC} target="_blank" rel="noreferrer">
+            <Typography color="primary.contrastText">©NUS SoC</Typography>
+          </Link>
+          <Link href={ORBITAL_WORDPRESS} target="_blank" rel="noreferrer">
+            <Typography color="primary.contrastText">
+              Orbital Program
+            </Typography>
+          </Link>
+          <Link href={SKYLAB_FRONTEND_ISSUES} target="_blank" rel="noreferrer">
+            <Typography color="primary.contrastText">Report Issues</Typography>
+          </Link>
         </Stack>
       </Container>
     </Box>
