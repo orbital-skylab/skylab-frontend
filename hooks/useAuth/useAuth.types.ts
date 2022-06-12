aa1820b99266b8de2efa383c5ab72a6cfb648e0b
myc37 <nvjn37@gmail.com>
2022-06-13 04:58:07 +0800

    chore: abstract useAuth types

---
+++ hooks/useAuth/useAuth.types.ts
@@ -0,0 +1,40 @@
+export type SimpleUser = {
+  email: string;
+  password: string;
+};
+
+export type User = {
+  email: string;
+  password: string;
+  name?: string;
+  profilePicUrl?: string;
+  githubUrl?: string;
+  linkedinUrl?: string;
+  personalSiteUrl?: string;
+  selfIntro?: string;
+};
+
+export interface IAuth {
+  user: User | null;
+  loading: boolean;
+  signUp: ({
+    name,
+    email,
+    password,
+    matricNo,
+    nusnetId,
+    cohortYear,
+    role,
+  }: {
+    name?: string;
+    email: string;
+    password: string;
+    matricNo?: string;
+    nusnetId?: string;
+    cohortYear: number;
+    role: "students" | "mentors" | "advisers";
+  }) => Promise<SimpleUser>;
+  signIn: (email: string, password: string) => Promise<void>;
+  logOut: () => Promise<void>;
+  resetPassword: (email: string) => Promise<void>;
+}
