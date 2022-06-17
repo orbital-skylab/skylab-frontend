53b68597a5e8faf6e7693baa7b6e3bd05d2e472c
myc37 <nvjn37@gmail.com>
2022-06-18 02:15:33 +0800

    feat: update useAuth to cookie authentication

--- hooks/useAuth/useAuth.tsx
+++ hooks/useAuth/useAuth.tsx
@@ -1,15 +1,17 @@
 import { ApiServiceBuilder } from "@/helpers/api";
 import { PAGES } from "@/helpers/navigation";
 import { HTTP_METHOD } from "@/types/api";
+import { IAuth, User } from "@/types/useAuth";
 import { useRouter } from "next/router";
-import { createContext, useContext, useEffect, useMemo, useState } from "react";
-import { IAuth, SimpleUser, User } from "./useAuth.types";
+import { createContext, useContext, useMemo, useState } from "react";
+
+const defaultUser: User = { email: "" };
 
 const AuthContext = createContext<IAuth>({
-  user: null,
+  user: defaultUser,
   loading: false,
   signUp: async () => {
-    return { email: "", password: "" }; /* Placeholder for callback function */
+    /* Placeholder for callback function */
   },
   signIn: async () => {
     /* Placeholder for callback function */
@@ -27,33 +29,9 @@ interface AuthProviderProps {
 }
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<User>(defaultUser);
   const [loading, setLoading] = useState<boolean>(true);
   const router = useRouter();
-
-  useEffect(() => {
-    const checkUserAuthorization = async (email: string) => {
-      const token = localStorage.getItem("jwt_token");
-      if (!token) return setUser(null);
-
-      const apiServiceBuilder = new ApiServiceBuilder({
-        method: HTTP_METHOD.GET,
-        endpoint: `/users/${email}`,
-        token,
-      });
-      const apiService = apiServiceBuilder.build();
-
-      const response = await apiService();
-      if (!response.ok) setUser(null);
-
-      const user = await response.json();
-      setUser(user as User);
-    };
-
-    if (user) {
-      checkUserAuthorization(user.email);
-    }
-  }, [user]);
 
   /**
    * TODO: ONLY FOR TESTING PURPOSES
@@ -96,20 +74,14 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
     const createUserResponse = await apiService();
 
     /**
-     * postgres user creation failed
+     * throw error if postgres user creation failed
      */
     if (!createUserResponse.ok) {
       const errorMessage = await createUserResponse.text();
-      setLoading(false);
       throw new Error(errorMessage);
     }
 
-    /**
-     * successful user creation returns the users' randomly generated password
-     */
-    const createUserResponseJson = await createUserResponse.json();
     setLoading(false);
-    return createUserResponseJson as SimpleUser;
   };
 
   const signIn = async (email: string, password: string) => {
@@ -141,8 +113,8 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
 
   const logOut = async () => {
     setLoading(true);
-    setUser(null);
-    localStorage.removeItem("jwt_token");
+    // TODO: make cookie expire
+    setUser(defaultUser);
     setLoading(false);
     router.push(PAGES.LANDING);
   };
