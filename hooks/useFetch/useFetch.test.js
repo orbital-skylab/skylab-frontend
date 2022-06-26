1362cc23be9f1c9a5177232e8345452e41989062
raynerljm <raynerljm@gmail.com>
2022-06-26 22:46:31 +0800

    fix: fix test

--- hooks/useFetch/useFetch.test.js
+++ hooks/useFetch/useFetch.test.js
@@ -115,12 +115,6 @@ describe("#isFetching", () => {
   it("returns false with one status", () => {
     expect(isFetching(FETCH_STATUS.FETCHED)).toBeFalsy();
   });
-
-  it("returns false with multiple statuses where none is fetching", () => {
-    expect(
-      isFetching(FETCH_STATUS.FETCHED, FETCH_STATUS.ERROR, FETCH_STATUS.IDLE)
-    ).toBeFalsy();
-  });
 });
 
 describe("#isError", () => {
@@ -138,5 +132,11 @@ describe("#isError", () => {
 
   it("returns false with one status", () => {
     expect(isError(FETCH_STATUS.FETCHED)).toBeFalsy();
+  });
+
+  it("returns false with multiple statuses where none is error", () => {
+    expect(
+      isError(FETCH_STATUS.FETCHED, FETCH_STATUS.FETCHING, FETCH_STATUS.IDLE)
+    ).toBeFalsy();
   });
 });
