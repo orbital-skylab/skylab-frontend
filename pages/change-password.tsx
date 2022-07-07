deb77b49bfe94cfde2a454417ead769b6d046f73
raynerljm <raynerljm@gmail.com>
2022-07-07 15:31:30 +0800

    feat: add redirect after changing password

--- pages/change-password.tsx
+++ pages/change-password.tsx
@@ -3,16 +3,27 @@ import type { NextPage } from "next";
 import Body from "@/components/layout/Body";
 import TextInput from "@/components/formikFormControllers/TextInput";
 import SnackbarAlert from "@/components/SnackbarAlert";
-import { Button, Container, Divider, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Divider,
+  Stack,
+  Typography,
+} from "@mui/material";
+import NoDataWrapper from "@/components/wrappers/NoDataWrapper";
+import NoneFound from "@/components/emptyStates/NoneFound";
 // Helpers
 import { Formik, FormikHelpers } from "formik";
 import * as Yup from "yup";
 // Hooks
 import useAuth from "@/hooks/useAuth";
 import useSnackbarAlert from "@/hooks/useSnackbarAlert";
+import { useRouter } from "next/router";
 // Constants
 import { ERRORS } from "@/helpers/errors";
-import { useRouter } from "next/router";
+import Link from "next/link";
+import { PAGES } from "@/helpers/navigation";
 
 interface ChangePasswordFormValuesType {
   newPassword: string;
@@ -42,7 +53,7 @@ const ChangePassword: NextPage = () => {
         typeof id !== "string"
       ) {
         throw new Error(
-          "The reset password link is invalid. Please try to reset your password again"
+          "The link is invalid. Please try to reset your password again"
         );
       }
 
@@ -51,7 +62,10 @@ const ChangePassword: NextPage = () => {
         token,
         id: parseInt(id, 10),
       });
-      setSuccess("Successfully changed password!");
+      setSuccess("Successfully changed password! You can now sign in.");
+      setTimeout(() => {
+        router.push(PAGES.LANDING);
+      }, 3000);
       helpers.resetForm();
     } catch (error) {
       setError(error);
@@ -62,46 +76,80 @@ const ChangePassword: NextPage = () => {
     <>
       <SnackbarAlert snackbar={snackbar} handleClose={handleClose} />
       <Body sx={{ display: "grid", placeItems: "center" }}>
-        <Container maxWidth="xs">
-          <Stack gap="1rem" justifyContent="center">
-            <Typography variant="h6" fontWeight={600}>
-              Change Password
-            </Typography>
-            <Formik
-              initialValues={initialValues}
-              onSubmit={handleSubmit}
-              validationSchema={changePasswordValidationSchema}
-            >
-              {(formik) => (
-                <form onSubmit={formik.handleSubmit}>
-                  <Stack gap="1rem">
-                    <TextInput
-                      label="New Password"
-                      name="newPassword"
-                      type="password"
-                      formik={formik}
-                    />
-                    <TextInput
-                      label="Confirm New Password"
-                      name="confirmNewPassword"
-                      type="password"
-                      formik={formik}
-                    />
+        <NoDataWrapper
+          noDataCondition={!token || !id}
+          fallback={
+            <NoneFound
+              message="The link is invalid. If you are trying to reset your password, please try again."
+              showReturnHome
+            />
+          }
+        >
+          <Container maxWidth="xs">
+            <Stack gap="1rem" justifyContent="center">
+              <Typography variant="h6" fontWeight={600}>
+                Change Password
+              </Typography>
+              <Formik
+                initialValues={initialValues}
+                onSubmit={handleSubmit}
+                validationSchema={changePasswordValidationSchema}
+              >
+                {(formik) => (
+                  <form onSubmit={formik.handleSubmit}>
+                    <Stack gap="1rem">
+                      <TextInput
+                        label="New Password"
+                        name="newPassword"
+                        type="password"
+                        formik={formik}
+                      />
+                      <TextInput
+                        label="Confirm New Password"
+                        name="confirmNewPassword"
+                        type="password"
+                        formik={formik}
+                      />
 
-                    <Button
-                      variant="contained"
-                      disabled={formik.isSubmitting}
-                      type="submit"
-                    >
-                      Change Password
-                    </Button>
-                  </Stack>
-                </form>
-              )}
-            </Formik>
-            <Divider />
-          </Stack>
-        </Container>
+                      <Button
+                        variant="contained"
+                        disabled={formik.isSubmitting}
+                        type="submit"
+                      >
+                        Change Password
+                      </Button>
+                    </Stack>
+                  </form>
+                )}
+              </Formik>
+              <Divider />
+              <Box>
+                <Typography
+                  textAlign="left"
+                  variant="subtitle2"
+                  fontWeight={600}
+                >
+                  Already have an account?
+                </Typography>
+                <Link href={PAGES.LANDING} passHref>
+                  <Typography
+                    variant="subtitle2"
+                    sx={{
+                      "&:hover": {
+                        color: "secondary.main",
+                        textDecoration: "underline",
+                        cursor: "pointer",
+                        transitionDuration: "150ms",
+                      },
+                    }}
+                  >
+                    Sign In
+                  </Typography>
+                </Link>
+              </Box>
+            </Stack>
+          </Container>
+        </NoDataWrapper>
       </Body>
     </>
   );
