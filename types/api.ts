17c6b17d44f45a54d7cf208241c7c4fc93ee89a7
raynerljm <raynerljm@gmail.com>
2022-07-20 19:07:13 +0800

    feat: update types for submission and api

--- types/api.ts
+++ types/api.ts
@@ -8,6 +8,7 @@ import {
   MentorRole,
   StudentRole,
 } from "./roles";
+import { Submission } from "./submissions";
 import { User, UserMetadata } from "./users";
 
 export enum HTTP_METHOD {
@@ -143,6 +144,12 @@ export type GetDeadlineDetailsResponse = {
 export type CreateDeadlineResponse = {
   deadline: Deadline;
 };
+
+/**
+ * Submissions Endpoints:
+ * https://github.com/orbital-skylab/skylab-backend/wiki/Submissions-Endpoints
+ */
+export type GetSubmissionResponse = { submission: Submission };
 
 /**
  * Error
