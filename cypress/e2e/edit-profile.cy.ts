6f96ae87f5f9460d9ead9933434c6c7fb2d5a993
myc37 <nvjn37@gmail.com>
2023-02-16 02:11:05 +0800

    feat: update e2e test for edit profile page

--- cypress/e2e/edit-profile.cy.ts
+++ cypress/e2e/edit-profile.cy.ts
@@ -1,102 +1,156 @@
 /* eslint-disable no-undef */
 /// <reference types="cypress" />
 
-context("Actions", () => {
-  before(() => {
-    cy.visit("http://localhost:3000/");
+context("User flow for editing profile", function () {
+  before("login", function () {
+    Cypress.session.clearAllSavedSessions();
+    cy.login("admin");
   });
 
-  it("sign in", () => {
-    cy.get("#sign-in-email-input").type("admin@skylab.com");
-    cy.get("#sign-in-password-input").type("Password123");
-    cy.get("#sign-in-button").click();
-    cy.wait(3000);
+  beforeEach(function () {
+    cy.setCookie("token", window.sessionStorage.getItem("token") ?? "");
+    cy.fixture("editProfile").then((fixture) => (this.fixture = fixture));
   });
 
-  it("navigate to profile page", () => {
+  // pre-edits
+
+  it("navigate to profile page", function () {
     cy.get("#nav-profile").click();
     cy.location("pathname").should("include", "users/");
   });
 
-  it("edit profile", () => {
+  it("check profile before editing", function () {
+    cy.get("#profile-name-span").should("have.text", this.fixture.originalName);
+    cy.get("#profile-picture-div").should("not.exist");
+    cy.get("#profile-github-link").should("not.exist");
+    cy.get("#profile-linkedin-link").should("not.exist");
+    cy.get("#profile-personal-site-link").should("not.exist");
+    cy.get("#profile-self-intro-span").should("not.exist");
+  });
+
+  // no edits
+
+  it("edit profile", function () {
     cy.get("#edit-profile-button").click();
     cy.location("pathname").should("include", "edit");
   });
 
-  it("update name", () => {
-    cy.get("#edit-name-input").type("Admin Name");
-  });
-
-  it("update profile picture", () => {
-    cy.get("#edit-profile-picture-input").type(
-      "https://loremflickr.com/640/480"
-    );
-  });
-
-  it("update github", () => {
-    cy.get("#edit-github-input").type("https://frozen-page.biz");
-  });
-
-  it("update linkedin", () => {
-    cy.get("#edit-linkedin-input").type("http://incredible-weedkiller.info");
-  });
-
-  it("update personal site", () => {
-    cy.get("#edit-personal-site-input").type("http://queasy-dispute.info");
-  });
-
-  it("update self intro", () => {
-    cy.get("#edit-self-intro-input").type(
-      "Perspiciatis atque libero architecto aliquam"
-    );
-  });
-
-  it("save changes", () => {
+  it("save changes without editing", function () {
     cy.get("#save-profile-button").click();
     cy.wait(3000);
     cy.go("back");
+    cy.location("pathname").should("not.include", "edit");
   });
 
-  it("check name", () => {
-    cy.get("#profile-name-span").should("have.text", "Admin Name");
+  it("check no changes made to profile", function () {
+    cy.get("#profile-name-span").should("have.text", this.fixture.originalName);
+    cy.get("#profile-picture-div").should("not.exist");
+    cy.get("#profile-github-link").should("not.exist");
+    cy.get("#profile-linkedin-link").should("not.exist");
+    cy.get("#profile-personal-site-link").should("not.exist");
+    cy.get("#profile-self-intro-span").should("not.exist");
   });
 
-  it("check profile picture", () => {
+  // valid edits
+
+  it("edit profile", function () {
+    cy.get("#edit-profile-button").click();
+    cy.location("pathname").should("include", "edit");
+  });
+
+  it("update name", function () {
+    cy.get("#edit-name-input").type(this.fixture.validName);
+  });
+
+  it("update profile picture", function () {
+    cy.get("#edit-profile-picture-input").type(
+      this.fixture.validProfilePicture
+    );
+  });
+
+  it("update github", function () {
+    cy.get("#edit-github-input").type(this.fixture.validGithub);
+  });
+
+  it("update linkedin", function () {
+    cy.get("#edit-linkedin-input").type(this.fixture.validLinkedIn);
+  });
+
+  it("update personal site", function () {
+    cy.get("#edit-personal-site-input").type(this.fixture.validPersonalSite);
+  });
+
+  it("update self intro", function () {
+    cy.get("#edit-self-intro-input").type(this.fixture.validSelfIntro);
+  });
+
+  it("save changes", function () {
+    cy.get("#save-profile-button").click();
+    cy.wait(3000);
+    cy.go("back");
+    cy.location("pathname").should("not.include", "edit");
+  });
+
+  it("check name", function () {
+    cy.get("#profile-name-span").should("have.text", this.fixture.validName);
+  });
+
+  it("check profile picture", function () {
     cy.get("#profile-picture-div")
       .find("img")
-      .should("have.attr", "src", "https://loremflickr.com/640/480");
+      .should("have.attr", "src", this.fixture.validProfilePicture);
   });
 
-  it("check github", () => {
+  it("check github", function () {
     cy.get("#profile-github-link").should(
       "have.text",
-      "https://frozen-page.biz"
+      this.fixture.validGithub
     );
   });
 
-  it("check linkedin", () => {
+  it("check linkedin", function () {
     cy.get("#profile-linkedin-link").should(
       "have.text",
-      "http://incredible-weedkiller.info"
+      this.fixture.validLinkedIn
     );
   });
 
-  it("check personal site", () => {
+  it("check personal site", function () {
     cy.get("#profile-personal-site-link").should(
       "have.text",
-      "http://queasy-dispute.info"
+      this.fixture.validPersonalSite
     );
   });
 
-  it("check self intro", () => {
+  it("check self intro", function () {
     cy.get("#profile-self-intro-span").should(
       "have.text",
-      '"Perspiciatis atque libero architecto aliquam"'
+      this.fixture.validSelfIntro
     );
   });
 
-  it("sign out", () => {
+  // invalid edits
+
+  it("edit profile", function () {
+    cy.get("#edit-profile-button").click();
+    cy.location("pathname").should("include", "edit");
+  });
+
+  it("update invalid name", function () {
+    cy.get("#edit-name-input").type(this.fixture.invalidName);
+  });
+
+  it("check not allowed to save changes", function () {
+    cy.get("#save-profile-button").should("be.disabled");
+    cy.go("back");
+    cy.location("pathname").should("not.include", "edit");
+  });
+
+  // done
+
+  it("sign out", function () {
     cy.get("#nav-sign-out").click();
+    cy.location("pathname").should("not.include", "users/");
   });
 });
 
