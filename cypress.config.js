cf661183db128473080ed8adc7084d9adf0cacb7
N Vijay Narayanan <nvjn37@gmail.com>
2023-08-29 23:51:05 +0800

    chore: update cypress config and commands

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  video: false,
+  watchForFileChanges: false,
   e2e: {
+    baseUrl: "http://localhost:4000/api/",
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     setupNodeEvents(on, config) {
       // implement node event listeners here
