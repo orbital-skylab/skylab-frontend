580ca1dc313a5d324ee8acef96c541468b9176f8
N Vijay Narayanan <nvjn37@gmail.com>
2023-10-03 17:22:15 +0800

    test: update student dashboard test suite

--- cypress/e2e/dashboard/student-dashboard.cy.js
+++ cypress/e2e/dashboard/student-dashboard.cy.js
@@ -4,22 +4,26 @@ describe("Testing student dashboard feature", () => {
   beforeEach(() => {
     cy.login("student@skylab.com", "Password123");
     cy.visit("http://localhost:3000/");
-  });
 
-  it("Submits deadline as a student", () => {
     // navigate to dashboard page
     cy.get("#nav-dashboard").click();
     cy.location("pathname").should("include", "/dashboard");
+  });
 
+  afterEach(() => {
+    cy.get("#sign-out-button").click();
+  });
+
+  it("Submits and edits deadline as a student", () => {
     // start a deadline
     cy.contains("td", "Milestone 2")
       .parent()
       .find("#start-deadline-button")
       .click();
-    cy.location("pathname").should("include", "deadline");
+    cy.location("pathname").should("include", "deadline/");
 
     // fill out and submit deadline
-    cy.get("").type("dummy answer for Milestone 2");
+    cy.get(".short-answer-input").first().type("dummy answer for Milestone 2");
     cy.get("#submit-deadline-button").click();
     cy.wait(3000);
     cy.go("back");
@@ -29,8 +33,10 @@ describe("Testing student dashboard feature", () => {
       .parent()
       .find("#edit-deadline-button")
       .click();
-    cy.get("").should("include.text", "dummy answer for Milestone 2");
-    cy.get("").type("edited dummy answer for Milestone 2");
+    cy.get(".short-answer-input")
+      .first()
+      .should("include.text", "dummy answer for Milestone 2")
+      .type("edited dummy answer for Milestone 2");
     cy.get("#submit-deadline-button").click();
     cy.wait(3000);
     cy.go("back");
@@ -40,7 +46,9 @@ describe("Testing student dashboard feature", () => {
       .parent()
       .find("#edit-deadline-button")
       .click();
-    cy.get("").should("include.text", "edited dummy answer for Milestone 2");
+    cy.get(".short-answer-input")
+      .first()
+      .should("include.text", "edited dummy answer for Milestone 2");
     cy.go("back");
 
     // start a deadline draft
@@ -48,10 +56,11 @@ describe("Testing student dashboard feature", () => {
       .parent()
       .find("#start-deadline-button")
       .click();
-    cy.location("pathname").should("include", "deadline");
 
     // fill out and save deadline as draft
-    cy.get("").type("dummy draft answer for Milestone 3");
+    cy.get(".short-answer-input")
+      .first()
+      .type("dummy draft answer for Milestone 3");
     cy.get("#save-draft-button").click();
     cy.wait(3000);
     cy.go("back");
@@ -61,7 +70,17 @@ describe("Testing student dashboard feature", () => {
       .parent()
       .find("#continue-deadline-button")
       .click();
-    cy.location("pathname").should("include", "deadline");
+    cy.location("pathname").should("include", "deadline/");
     cy.get("").should("include.text", "dummy draft answer for Milestone 3");
+  });
+
+  it("Views evaluations as a student", () => {
+    cy.get("#received-evaluations-tab").click();
+    cy.get(".view-evaluation-button").first().click();
+    cy.location("pathname").should("include", "submissions/");
+
+    cy.go("back");
+    cy.get(".view-anonymous-answers").first().click();
+    cy.location("pathname").should("include", "submissions/student/");
   });
 });
