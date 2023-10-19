2ec5d08f7424a533930aaa567a36f2cd5b24de02
N Vijay Narayanan <nvjn37@gmail.com>
2023-10-19 15:29:49 +0800

    chore: remove type command overwrite

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,10 +1,4 @@
 /* eslint-disable no-undef */
-Cypress.Commands.overwrite("type", (originalFn, element, text, options) => {
-  const clearedText = `{selectall}{backspace}${text}`;
-
-  return originalFn(element, clearedText, options);
-});
-
 Cypress.Commands.add("login", (email, password) => {
   cy.request("POST", "/auth/sign-in", {
     email,
