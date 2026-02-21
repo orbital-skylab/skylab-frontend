b0ef0c6b216ae3fb6f5fd358af62218e540d2030
sharonsohxuanhui@hotmail.com <sharonsohxuanhui@hotmail.com>
2026-02-22 01:44:50 +0800

    fix: remove redundant conditional statement

--- components/questions/QuestionCard/MultipleChoiceQuestion/MultipleChoiceQuestion.tsx
+++ components/questions/QuestionCard/MultipleChoiceQuestion/MultipleChoiceQuestion.tsx
@@ -47,7 +47,7 @@ const MultipleChoiceQuestion: FC<Props> = ({
           border: hasError ? "1px solid" : "1px solid transparent",
           borderColor: hasError ? "error.main" : "transparent",
           borderRadius: "4px",
-          padding: hasError ? "0.5rem" : "0.5rem",
+          padding: "0.5rem",
           marginLeft: "-0.5rem",
         }}
       >
--- types/deadlines.ts
+++ types/deadlines.ts
@@ -57,7 +57,7 @@ export type Question = {
   type: QUESTION_TYPE;
   options?: Option[];
   isAnonymous?: boolean;
-  isRequired: boolean;
+  isRequired?: boolean;
 };
 
 export type LeanQuestion = Omit<
