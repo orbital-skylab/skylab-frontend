2b303403d92b88d8a4ffe323cf086db6f77e9c1c
sharonsohxuanhui@hotmail.com <sharonsohxuanhui@hotmail.com>
2026-03-08 22:33:05 +0800

    refactor: extract AnonymousQuestionSectionsList into a shared component

---
+++ components/questions/AnonymousQuestionSectionsList/AnonymousQuestionSectionsList.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import QuestionSectionsList from "@/components/questions/QuestionSectionsList";
+import useAnswers from "@/hooks/useAnswers";
+import { Section } from "@/types/deadlines";
+import { Answer } from "@/types/submissions";
+
+const AnonymousQuestionSectionsList = ({
+  questionSections,
+  answersArray,
+}: {
+  questionSections: Section[];
+  answersArray: Answer[];
+}) => {
+  const { answers, actions } = useAnswers();
+  const [hasLoadedAnswers, setHasLoadedAnswers] = useState(false);
+
+  useEffect(() => {
+    if (!hasLoadedAnswers && answersArray && answersArray.length > 0) {
+      actions.setAnswersFromArray(answersArray);
+      setHasLoadedAnswers(true);
+    }
+  }, [actions, answersArray, hasLoadedAnswers]);
+
+  if (!hasLoadedAnswers) {
+    return null;
+  }
+
+  return (
+    <QuestionSectionsList
+      questionSections={questionSections}
+      answers={answers}
+      includeAnonymousQuestions
+      isReadonly={true}
+      answersActions={actions}
+    />
+  );
+};
+
+export default AnonymousQuestionSectionsList;
