39adb117259aa844327d6127cdc496502d3c0f60
sharonsohxuanhui@hotmail.com <sharonsohxuanhui@hotmail.com>
2026-04-16 13:30:31 +0800

    feat: add row rendering and action row jest tests

--- jest.config.js
+++ jest.config.js
@@ -15,6 +15,7 @@ const customJestConfig = {
   testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
     "^@/components/(.*)$": "<rootDir>/components/$1",
+    "^@/contexts/(.*)$": "<rootDir>/contexts/$1",
     "^@/types/(.*)$": "<rootDir>/types/$1",
     "^@/helpers/(.*)$": "<rootDir>/helpers/$1",
   },
